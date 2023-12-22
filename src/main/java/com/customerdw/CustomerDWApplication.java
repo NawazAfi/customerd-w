ee41883f2893c22d5242c790b17a44bf3d2c9699
NawazAfi <nawaznavvu0@example.com>
2023-12-22 16:45:51 +0530

    Initial commit

---
+++ src/main/java/com/customerdw/CustomerDWApplication.java
@@ -0,0 +1,13 @@
+package com.customerdw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerDWApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerDWApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/customerdw/CustomerDWApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customerdw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerDWApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
